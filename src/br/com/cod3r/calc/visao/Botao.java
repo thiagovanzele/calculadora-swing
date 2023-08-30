327823fbb20360aeabc4b52d64e91536a8dabe48
Thiago Vanzele <thiagovanzele@gmail.com>
2023-08-30 14:12:16 -0300

    Componente botao

--- src/br/com/cod3r/calc/visao/Botao.java
+++ src/br/com/cod3r/calc/visao/Botao.java
@@ -1,8 +1,22 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton {
+	
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setOpaque(true);
+		setBackground(cor);
+		setFont(new Font("Segoe", Font.PLAIN, 25));
+		setForeground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+	}
 
 }
--- src/br/com/cod3r/calc/visao/Teclado.java
+++ src/br/com/cod3r/calc/visao/Teclado.java
@@ -1,10 +1,43 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Teclado extends JPanel {
+	
+	private final Color CINZA_ESCURO = new Color(50, 50, 50, 255);
+	private final Color CINZA_CLARO = new Color(101, 101, 101, 255);
+	private final Color AZUL = new Color(118, 185, 238, 255);
 
 	public Teclado() {
+		setLayout(new GridLayout(5, 4));
+		
+		add(new Botao("AC", CINZA_ESCURO));
+		add(new Botao("+/-", CINZA_ESCURO));
+		add(new Botao("%", CINZA_ESCURO));
+		add(new Botao("/", AZUL));
+		
+		add(new Botao("7", CINZA_CLARO));
+		add(new Botao("8", CINZA_CLARO));
+		add(new Botao("9", CINZA_CLARO));
+		add(new Botao("X", AZUL));
+		
+		add(new Botao("7", CINZA_CLARO));
+		add(new Botao("8", CINZA_CLARO));
+		add(new Botao("9", CINZA_CLARO));
+		add(new Botao("X", AZUL));
+		
+		add(new Botao("7", CINZA_CLARO));
+		add(new Botao("8", CINZA_CLARO));
+		add(new Botao("9", CINZA_CLARO));
+		add(new Botao("X", AZUL));
+		
+		add(new Botao("7", CINZA_CLARO));
+		add(new Botao("8", CINZA_CLARO));
+		add(new Botao("9", CINZA_CLARO));
+		add(new Botao("X", AZUL));
 	}
 }
