517f698a91fe4eaed91a6013d815ec561e6f7968
Thiago Vanzele <thiagovanzele@gmail.com>
2023-08-30 14:01:28 -0300

    Ajustando o display

--- src/br/com/cod3r/calc/visao/Display.java
+++ src/br/com/cod3r/calc/visao/Display.java
@@ -1,11 +1,26 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 	
+	private final JLabel label;
+	
 	public Display() {
+		setBackground(new Color(32, 32, 32, 255));
+		label = new JLabel("1234,56");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Segoe", Font.PLAIN, 35));
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+		
+		add(label);
 	}
 
 }
